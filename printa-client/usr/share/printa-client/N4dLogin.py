71409a17d201a4c8fb8c71d69f81b1206d4ab003
cless <hectorgh@gmail.com>
2020-06-03 10:03:45 +0200

    adding new files

---
+++ printa-client/usr/share/printa-client/N4dLogin.py
@@ -0,0 +1,236 @@
+import os
+import xmlrpc.client
+import ssl
+import sys
+import socket
+
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+DEBUG=True
+
+def dprint(data):
+	
+	if DEBUG:
+		print("[N4dLogin] %s"%data)
+
+class N4dLogin:
+	
+	def __init__(self,server="server",force_get_auth=False):
+		
+		self.user=os.environ["USER"]
+		self.uid=os.getuid()
+		self.server=server
+		self.set_server(self.server)
+		self.ticket=None
+		
+		self.ip=self.get_own_ip(self.server)
+		
+		#STEP 1 - AUTHLESS
+		self.create_ticket()
+		#STEP 2 -  HOPE WE CAN READ A TICKET
+		self.ticket=self.read_ticket()
+		
+		if self.ticket==None:
+			# STEP 3 - BAD  LUCK. WE NEED AUTHENTICATION
+			if force_get_auth:
+				self.get_auth()
+		
+	#def __init__
+
+	
+	def set_server(self,server):
+		
+		context=ssl._create_unverified_context()	
+		self.client=xmlrpc.client.ServerProxy("https://%s:9779"%server,allow_none=True,context=context)
+		
+	#def set_server
+
+	
+	def get_own_ip(self,server):
+
+		ip = None
+		
+		try:
+			s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+			s.connect((server,9779))
+			ip,port=s.getsockname()
+		except Exception as e:
+			dprint(str(e))
+		
+		return ip
+		
+	#def get_own_ip
+	
+	def validate_user(self,u,p):
+		
+		dprint("Validating user...")
+		try:
+			status,groups=self.client.validate_user(u,p)
+			dprint(status)
+			if status:
+				return True
+			return False
+		except:
+			return False
+			
+	#def validate_user
+	
+	def validate_ticket(self,u,t):
+		
+		dprint("Validating ticket...")
+		try:
+			ret=self.client.validate_ticket((u,t),"PrintaServer")
+			return ret["status"]
+		except:
+			return False
+		
+	#def validate_ticket
+	
+	
+	def create_ticket(self):
+	
+		try:
+			dprint("Asking %s to create a ticket..."%self.server)
+			ret=self.client.create_ticket("","NTicketsManager",self.user)
+			return True
+		except:
+			return False
+	
+	#def create_ticket
+	
+	
+	def read_ticket_from_server(self,u,p):
+		try:
+			dprint("Reading remote ticket...")
+			t=self.client.get_ticket((u,p),"NTicketsManager",self.user)
+			remote_ticket_path="/run/user/%s/printa/%s.n4d"%(self.uid,self.user)
+			f=open(remote_ticket_path,"w")
+			f.write(t)
+			f.close()
+			os.chmod(remote_ticket_path,0o400)
+			return t
+	
+		except Exception as e:
+			dprint(str(e))
+			return False
+			
+	#def read_ticket_from_server
+	
+	
+	def read_ticket(self):
+		
+		#lets hope there is an available ticket
+		
+		dprint("Trying to read local tickets...")
+			
+		ticket_path="/run/n4d/tickets/%s"%self.user
+		remote_ticket_path="/run/user/%s/printa/%s.n4d"%(self.uid,self.user)
+
+		tickets=[]
+		if self.ip!="127.0.0.1":
+			dprint("Skiping self generated ticket...")
+		else:
+			tickets.append(ticket_path)
+			
+		tickets.append(remote_ticket_path)
+	
+		for ticket in tickets:
+			if os.path.exists(ticket):
+				f=open(ticket)
+				t=f.readline()
+				f.close()
+				dprint("Ticket found in %s"%ticket)
+				if self.validate_ticket(self.user,t):
+					return t
+					
+		return None
+		
+	#def read_ticket
+	
+	
+	def run_dialog(self,alert_msg=""):
+		
+		def manual_validate(entry,dialog):
+		
+			dialog.hide()
+		
+		#def manual_validate
+		
+		w=Gtk.Window()
+		
+		dialog = Gtk.Dialog("Printa",w,Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+		(Gtk.STOCK_CANCEL, Gtk.ResponseType.REJECT,Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT))
+		
+		img=Gtk.Image()
+		img.set_from_file('printa.svg')
+		img.show()
+		dialog.vbox.pack_start(img,False,True,5)
+
+		hbox1=Gtk.Table(2,2)
+		hbox1.set_row_spacings(5)
+		hbox1.set_col_spacings(5)
+		hbox1.set_border_width(5)
+		user_entry=Gtk.Entry()
+		user_entry.set_text(self.user)
+		user_entry.set_sensitive(False)
+		user_label=Gtk.Label("User")
+		user_label.set_alignment(0,0)
+		password_entry=Gtk.Entry()
+		password_entry.set_visibility(False)
+		password_entry.connect("activate",manual_validate,dialog)
+		password_label=Gtk.Label("Password")
+		password_label.set_alignment(0,0)
+		hbox1.attach(user_label,0,1,0,1)
+		hbox1.attach(user_entry,1,2,0,1)
+		hbox1.attach(password_label,0,1,1,2)
+		hbox1.attach(password_entry,1,2,1,2)
+		dialog.vbox.pack_start(hbox1,False,True,5)
+		msg_label=Gtk.Label()
+		if alert_msg!="":
+			skel_msg="<span foreground='red'>%s</span>"%alert_msg
+			msg_label.set_markup(skel_msg)
+		dialog.vbox.pack_start(msg_label,True,True,5)
+		dialog.vbox.show_all()
+		password_entry.grab_focus()
+		dialog.present()
+		response = dialog.run()
+		dialog.hide()
+		
+		ret={}
+		ret["response"]=response
+		ret["user"]=user_entry.get_text()
+		ret["password"]=password_entry.get_text()
+		
+		return ret
+		
+	#def run_dialog
+	
+	
+	def get_auth(self):
+		
+		#we don't have access to local ticket
+		try:
+			ret={}
+			ret=self.run_dialog()
+			validated=False
+			
+			while not validated and ( ret["response"]==-1 or ret["response"]==-3):
+				validated=self.validate_user(ret["user"],ret["password"])
+				if not validated:
+					ret=self.run_dialog("User and/or password error")
+			
+			if validated:
+				self.ticket=self.read_ticket_from_server(ret["user"],ret["password"])
+			else:
+				dprint("Exiting")
+				
+		except Exception as e:
+			print(e)
+		
+	#def ask_for_ticket
+	
+	
+#class N4dLogin
---
+++ printa-client/usr/share/printa-client/printa-client.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python3
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Wnck','3.0')
+from gi.repository import Gtk,GObject,Gio,Wnck,GdkX11
+
+import threading 
+import time
+import os
+import pwd
+import socket
+import xmlrpc.client
+import ssl
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+import N4dLogin
+
+
+DEBUG=True
+RSRC_PATH="./"
+
+def dprint(data):
+	
+	if DEBUG:
+		print("[PrintaClient] %s"%data)
+
+
+class PrintaClient:
+	
+	def __init__(self):
+		
+		self.remote_server=self.get_printa_config_variable()
+		self.n4d_login=N4dLogin.N4dLogin(self.remote_server)
+		
+		self.window_ref=None
+		self.processed_ids=[]
+		self.uid=os.geteuid()
+		self.user=pwd.getpwuid(self.uid).pw_name
+		self.dpath="/run/user/%s/printa"%os.getuid()
+		if not os.path.exists(self.dpath):
+			os.makedirs(self.dpath)
+		self.token_path=self.dpath+"/printa_client"
+		dprint(self.token_path)
+		
+		if self.remote_server==None:
+			self.remote_server="127.0.0.1"
+		self.ip=self.get_own_ip()
+		
+		self.create_path_file()
+		self.init_gio_watcher()
+
+		GObject.threads_init()
+		self.start_gui()
+		
+	#def init
+	
+	# ######################
+	
+	# N4D FUNCTIONS 
+	# ######################
+
+	def commit_job(self,status):
+		
+		if self.n4d_login.ticket!=None:
+		
+			self.job["status"]=status
+			context=ssl._create_unverified_context()
+			c=xmlrpc.client.ServerProxy("https://%s:9779"%self.remote_server,allow_none=True,context=context)
+			u=(self.n4d_login.user,self.n4d_login.ticket)
+			ret=c.commit_request(u,"PrintaServer",self.ip,self.job["user"],self.job["id"],self.job["status"])
+			dprint(ret)
+		
+	#def commit_job
+
+	def is_job_printable(self):
+	
+		USER_LOCKED=-1
+		NOT_ENOUGH_QUOTA=-2
+		FREE_PASS=1
+		ENOUGH_QUOTA=2
+	
+		context=ssl._create_unverified_context()
+		c=xmlrpc.client.ServerProxy("https://%s:9779"%self.remote_server,allow_none=True,context=context)
+		u=(self.n4d_login.user,self.n4d_login.ticket)
+		ret=c.is_job_printable(u,"PrintaServer","",self.job)
+		dprint("IS_JOB_PRINTABLE:")
+		dprint(ret)
+		can_print=ret["status"]
+		reason=""
+		if not can_print:
+			if ret["msg"]==USER_LOCKED:
+				reason="User is locked"
+			elif ret["msg"]==NOT_ENOUGH_QUOTA:
+				reason="Not enough quota"
+			else:
+				reason=ret["msg"]
+		
+		return [can_print,reason]
+		
+	#def is_job
+
+	def get_user_quota(self):
+		
+		context=ssl._create_unverified_context()
+		c=xmlrpc.client.ServerProxy("https://%s:9779"%self.remote_server,allow_none=True,context=context)
+		u=(self.n4d_login.user,self.n4d_login.ticket)
+		ret=c.get_user_quota(u,"PrintaServer","")
+		dprint("USER QUOTA:")
+		dprint(ret)
+		if ret["status"]:
+			return ret["msg"]
+		else:
+			return -1
+		
+	#def get_user_quota
+
+	def get_requests_variable(self):
+		
+		context=ssl._create_unverified_context()
+		c=xmlrpc.client.ServerProxy("https://%s:9779"%self.remote_server,allow_none=True,context=context)
+		ret=c.get_request_var("","PrintaServer")
+		
+		return ret
+		
+	#def get_requests_variable
+	
+	def get_printa_config_variable(self):
+	
+		context=ssl._create_unverified_context()
+		c=xmlrpc.client.ServerProxy("https://localhost:9779",allow_none=True,context=context)
+		return c.get_variable("","VariablesManager","PRINTASERVER")
+		
+	#def get_printa_config_variable
+	
+	def validate_ticket(self):
+		
+		if self.n4d_login.ticket!=None:
+
+			context=ssl._create_unverified_context()
+			c=xmlrpc.client.ServerProxy("https://%s:9779"%self.remote_server,allow_none=True,context=context)
+			u=(self.n4d_login.user,self.n4d_login.ticket)
+			ret=c.validate_ticket(u,"PrintaServer")
+			
+			if type(ret)==dict:
+				if ret["status"]:
+					return True
+				
+		# not a valid ticket. Requesting a new one and forcing user and password dialog
+		self.n4d_login=N4dLogin.N4dLogin(self.remote_server,True)
+		
+	#def validate_ticket
+	
+	# ######################## #
+
+
+	#  TOKEN WATCHING FUNCTIONS
+	# ##########################
+	
+	def init_gio_watcher(self):
+		
+		gio_file=Gio.File.new_for_path(self.token_path)
+		self.monitor=gio_file.monitor_file(Gio.FileMonitorFlags.NONE,None)
+		self.monitor.connect("changed",self.file_changed)
+	
+	#def init_gio_watcher
+	
+	
+	def file_changed(self,monitor,file,other_file,event):
+
+		if event == Gio.FileMonitorEvent.CHANGES_DONE_HINT:
+			self.process_msg_variable()
+		
+	#def file_changed
+	
+	def process_msg_variable(self):
+		
+		dprint("Got PRINTAREQUESTS variable change...")
+		
+		ret=self.get_requests_variable()
+
+		if ret["status"]:
+			v=ret["msg"]
+		
+		msgs=[]
+		
+		show_window=False
+	
+		if self.ip in v:
+			if self.user in v[self.ip]:
+				for request in v[self.ip][self.user]:
+					if request["job_info"]["time"]>=self.last_check and request["job_info"]["status"]=="waiting" and request["job_info"]["id"] not in self.processed_ids:
+						dprint(request)
+						self.validate_ticket()
+						
+						self.msg_label.set_text("")
+						self.job_name.set_text(request["job_info"]["job_name"])
+						self.printer.set_text(request["job_info"]["printer_name"])
+						if "estimated_pages" in request["job_info"]:
+							self.pages.set_text(str(request["job_info"]["estimated_pages"]))
+						else:
+							self.pages.set_text(str(request["job_info"]["pages"]))
+						if self.n4d_login.ticket!=None:
+							user_quota=str(self.get_user_quota())
+						else:
+							user_quota="???"
+						self.quota_label.set_text(user_quota)
+						self.job=request["job_info"]
+						show_window=True
+						self.processed_ids.append(self.job["id"])
+		
+		if show_window:
+			dprint("Requesting window...")
+			
+			if self.n4d_login.ticket!=None:
+			
+				ret=self.is_job_printable()
+				dprint(ret)
+				can_print,reason=ret
+				self.print_button.set_sensitive(can_print)
+			
+				if not can_print:
+					skel_msg="<span foreground='red'>Job can't be printed: %s</span>"%reason
+					self.msg_label.set_markup(skel_msg)
+				
+			else:
+				skel_msg="<span foreground='red'>Authentication error. Job can't be printed</span>"
+				self.msg_label.set_markup(skel_msg)
+				self.print_button.set_sensitive(False)
+				
+			self.w.show_all()
+			self.w.present()
+			
+	#def process_msg_variable
+	
+	# ############################
+
+	# BASIC FUNCTIONS
+	# ###################
+	
+	def get_own_ip(self):
+
+		ip = None
+		
+		try:
+			s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+			s.connect((self.remote_server,9779))
+			ip,port=s.getsockname()
+		except Exception as e:
+			print(e)
+		
+		dprint("IP: %s"%ip)
+		return ip
+		
+	#def get_own_ip
+	
+	def create_path_file(self):
+		
+		dprint("Generating timestamp token...")
+		self.last_check=time.time()
+		f=open(self.token_path,"w")
+		f.write(str(self.last_check))
+		f.close()
+		
+	#def create_path_file
+	
+	def start_gui(self):
+		
+		dprint("Initializing gui...")
+		
+		builder=Gtk.Builder()
+		builder.add_from_file(RSRC_PATH+"printa-client.ui")
+		
+		self.print_button=builder.get_object("print_button")
+		self.cancel_button=builder.get_object("cancel_button")
+		self.w=builder.get_object("window2")
+		self.job_name=builder.get_object("label20")
+		self.printer=builder.get_object("label21")
+		self.pages=builder.get_object("label22")
+		self.quota_label=builder.get_object("label26")
+		self.print_button=builder.get_object("button4")
+		self.cancel_button=builder.get_object("button3")
+		self.msg_label=builder.get_object("label24")
+
+		self.connect_signals()
+		
+		Gtk.main()
+		
+	#def start_gui
+	
+	def connect_signals(self):
+		
+		self.print_button.connect("clicked",self.print_clicked)
+		self.cancel_button.connect("clicked",self.cancel_clicked)
+		self.w.connect("delete_event",self.hide_window)
+		self.w.connect("map-event",self.map_event)		
+		
+	#def connect_signals
+
+	
+	# ######################
+	
+	# SIGNAL FUNCTIONS
+	# ######################
+	
+	def hide_window(self,widget,event=None):
+		
+		dprint("Hiding window...")
+		self.w.hide()
+		return True
+		
+	#def hide_window
+	
+	def map_event(self,widget,event):
+		
+		dprint("Window is shown. Requesting focus...")
+		
+		xid=self.w.get_window().get_xid()
+		tstamp=GdkX11.x11_get_server_time(self.w.get_window())
+		
+		if self.window_ref==None:
+		
+			screen=Wnck.Screen.get_default()
+			screen.force_update()
+			
+			for w in screen.get_windows():
+				if w.get_xid()==xid:
+					self.window_ref=w
+					w.activate(tstamp)
+					break
+		
+		if self.window_ref !=None:
+			self.window_ref.activate(tstamp)
+			
+		self.w.present()
+		
+	#def map_event
+	
+	def print_clicked(self,widget):
+		
+		dprint("Accepting job...")
+		
+		# let's make sure we are still able to print
+		ret=self.is_job_printable()
+		dprint(ret)
+		can_print,reason=ret
+		if can_print:
+			self.commit_job("completed")
+			self.w.hide()
+		else:
+			skel_msg="<span foreground='red'>Job can't be printed: %s</span>"%reason
+			self.msg_label.set_markup(skel_msg)
+			widget.set_sensitive(False)
+			
+	#def print clicked
+	
+	def cancel_clicked(self,widget):
+		
+		dprint("Cancelling...")
+		self.commit_job("cancelled")
+		self.w.hide()
+		
+	#def cancel clicked
+	
+	
+#class PrintaClient
+
+if __name__=="__main__":
+	pc=PrintaClient()
+	
