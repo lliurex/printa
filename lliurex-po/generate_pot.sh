00d26015edd5e0ceb09eaf67900fb596babda53e
daduve <daduve@gmail.com>
2020-07-22 02:13:34 +0200

    Adding translations to printa-users-manager

---
+++ lliurex-po/generate_pot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE_POT="printa-users-manager/printa-users-manager.pot"
+EXT=".ui"
+if [ ! -f $FILE_POT ]; then
+	touch $FILE_POT
+else
+	rm $FILE_POT
+	touch $FILE_POT
+fi
+
+xgettext ../printa-users-manager/usr/share/printa-users-manager/rsrc/printa-manager.ui -o $FILE_POT
+echo "Finished, you can review printa-users-manager/printa-users-manager.pot"
